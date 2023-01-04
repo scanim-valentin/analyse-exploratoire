e19cb0e6fcce12f4a4531b5e4d6b28c3d3b03fbb
scanim-valentin <valentin.guerbette@hotmail.fr>
2023-01-04 15:39:17 +0100

    xLike/yDislike (size=view_count)

--- exploration.R
+++ exploration.R
@@ -2,14 +2,16 @@
 library(tidyverse)
 
 # Diagramme 1 : Un diagramme de dispersion qui montre la relation entre le nombre de vues et le nombre de likes/dislikes pour chaque vidéo.
-dataframe = FR_youtube_trending_data[FR_youtube_trending_data$ratings_disabled=="False",] %>% group_by(video_id) %>% arrange(-view_count)
+dataframe = FR_youtube_trending_data[FR_youtube_trending_data$views>0,] %>% group_by(video_id) %>% arrange(-view_count)
 subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=likes,y=view_count)) + geom_point(shape=4)
 
 # Diagramme 2 : Un diagramme de dispersion qui montre la relation entre le nombre de vues et le nombre de commentaires pour chaque vidéo.
-dataframe = FR_youtube_trending_data[FR_youtube_trending_data$comments_disabled=="False",] %>% group_by(video_id) %>% arrange(-view_count)
+dataframe = FR_youtube_trending_data[FR_youtube_trending_data$comments_disabled=="False" && FR_youtube_trending_data$views > 0,] %>% group_by(video_id) %>% arrange(-view_count)
 subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=comment_count,y=view_count)) + geom_point(shape=4)
 
 # Diagramme 3 : 
+dataframe = FR_youtube_trending_data[FR_youtube_trending_data$ratings_disabled=="False",] %>% group_by(video_id) %>% arrange(-view_count)
+subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=likes,y=view_count)) + geom_point(shape=4)
 
 # Diagramme 4 : 
 
