51c8fc91431dbe66a3e4a838e3faed7e90d9e01d
“Jean6-971” <75419220+Jean6-971@users.noreply.github.com>
2023-01-04 12:16:39 +0100

    Update exploration.R

--- exploration.R
+++ exploration.R
@@ -1,8 +1,13 @@
 # Comment évaluer la qualité des vidéos en se basant sur les commentaires et les likes/dislikes ?
 library(tidyverse)
+
 # Diagramme 1 : Un diagramme de dispersion qui montre la relation entre le nombre de vues et le nombre de likes/dislikes pour chaque vidéo.
-FR_youtube_trending_data %>% arrange(view_count) %>% ggplot(aes(x=likes,y=view_count)) + geom_histogram(stat="identity")
-# Diagramme 2 : 
+dataframe = FR_youtube_trending_data[FR_youtube_trending_data$ratings_disabled=="False",] %>% group_by(video_id) %>% arrange(-view_count)
+subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=likes,y=view_count)) + geom_point(shape=4)
+
+# Diagramme 2 : Un diagramme de dispersion qui montre la relation entre le nombre de vues et le nombre de commentaires pour chaque vidéo.
+dataframe = FR_youtube_trending_data[FR_youtube_trending_data$comments_disabled=="False",] %>% group_by(video_id) %>% arrange(-view_count)
+subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=comment_count,y=view_count)) + geom_point(shape=4)
 
 # Diagramme 3 : 
 
