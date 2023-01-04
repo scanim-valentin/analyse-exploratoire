906300a22a69bd86eab12f3d1abf81345e43901b
scanim-valentin <valentin.guerbette@hotmail.fr>
2023-01-04 16:22:41 +0100

    commit

--- exploration.R
+++ exploration.R
@@ -1,18 +1,9 @@
 # Comment évaluer la qualité des vidéos en se basant sur les commentaires et les likes/dislikes ?
 library(tidyverse)
-
+# && FR_youtube_trending_data$dislikes > 0 && FR_youtube_trending_data$likes > 0
 # Diagramme 1 : Un diagramme de dispersion qui montre la relation entre le nombre de vues et le nombre de likes/dislikes pour chaque vidéo.
-dataframe = FR_youtube_trending_data[FR_youtube_trending_data$views>0,] %>% group_by(video_id) %>% arrange(-view_count)
-subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=likes,y=view_count)) + geom_point(shape=4)
+dataframe = FR_youtube_trending_data[FR_youtube_trending_data$dislikes > 0 ,] %>% group_by(video_id) %>% arrange(-view_count)
+dataframe2 = subset(dataframe,!duplicated(dataframe$video_id))
+p <- ggplot(dataframe2, aes(likes, dislikes, size = exp(view_count))) 
+p + geom_point() + scale_x_continuous(trans='log10') + scale_y_continuous(trans='log10')
 
-# Diagramme 2 : Un diagramme de dispersion qui montre la relation entre le nombre de vues et le nombre de commentaires pour chaque vidéo.
-dataframe = FR_youtube_trending_data[FR_youtube_trending_data$comments_disabled=="False" && FR_youtube_trending_data$views > 0,] %>% group_by(video_id) %>% arrange(-view_count)
-subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=comment_count,y=view_count)) + geom_point(shape=4)
-
-# Diagramme 3 : 
-dataframe = FR_youtube_trending_data[FR_youtube_trending_data$ratings_disabled=="False",] %>% group_by(video_id) %>% arrange(-view_count)
-subset(dataframe,!duplicated(dataframe$video_id)) %>% ggplot(aes(x=likes,y=view_count)) + geom_point(shape=4)
-
-# Diagramme 4 : 
-
-# Diagramme 5 : 
