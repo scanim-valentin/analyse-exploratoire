d8354fa7bf5ea3c709653757b81835369bda866b
“Jean6-971” <75419220+Jean6-971@users.noreply.github.com>
2023-01-05 16:56:06 +0100

    Create graphe3.R

---
+++ graphe3.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+
+dataframe1 = FR_youtube_trending_data %>%
+  group_by(video_id) %>%
+  arrange(-view_count)
+
+dataframe2 = subset(dataframe1,!duplicated(dataframe1$video_id)) %>%
+  select(categoryId, view_count, publishedAt, video_id) %>%
+  mutate(publishedAt = as.Date(publishedAt))
+
+dataframe2 = dataframe2[which(dataframe2$publishedAt<"2023-01-01"),] %>%
+  mutate(quarter = as.Date(cut(publishedAt, "quarter")))
+
+dataframe3 <- dataframe2 %>%
+  group_by(quarter, categoryId) %>%
+  summarise(mean_view_count = mean(view_count))
+
+dataframe4 = dataframe3 %>%
+  mutate(categoryId = as.factor(categoryId)) %>%
+  mutate(categoryId = fct_recode(categoryId,
+                                 "Film & Animation" = "1",
+                                 "Autos & Vehicles" = "2",
+                                 "Music" = "10",
+                                 "Pets & Animals" = "15",
+                                 "Sports" = "17",
+                                 "Travel & Events" = "19",
+                                 "Gaming" = "20",
+                                 "People & Blogs" = "22",
+                                 "Comedy" = "23",
+                                 "Entertainment" = "24",
+                                 "News & Politics" = "25",
+                                 "Howto & Style" = "26",
+                                 "Education" = "27",
+                                 "Science & Technology" = "28",
+                                 "Nonprofits & Activism" = "29"
+  ))
+
+ggplot(dataframe4, aes(x = quarter, y = mean_view_count, color = categoryId)) +
+  geom_line(size = 1.5) +
+  scale_x_date(date_breaks = "3 month", date_labels = "%b-%Y") +
+  theme(axis.text.x = element_text(angle = 40, vjust = 1, hjust=1, size=12, face="bold"),
+        axis.title.x = element_text(size = 18, face="bold"),
+        axis.text.y = element_text(size=12, face="bold"),
+        axis.title.y = element_text(size = 18, face="bold"),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.position = "none") +
+  labs(x = "Date de publication", y = "Vues moyennes par jour")
